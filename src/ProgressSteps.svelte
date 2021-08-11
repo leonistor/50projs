0386a26a53e1191a704fe77f48c99a402c3a004f
leonistor <leo.nistor@gmail.com>
2021-08-11 10:51:26 +0300

    work 02 progress steps

--- src/ProgressSteps.svelte
+++ src/ProgressSteps.svelte
@@ -1,17 +1,34 @@
 <script>
+  import Step from './Step.svelte'
+
+  let steps = ['Look', 'Touch', 'Kiss', 'Swallow']
+  let current = 0
+  let message = 'Please proceed'
 </script>
 
 <div class="content">
-  <div style="display: flex; flex-direction: column; align-items: center;">
+  <div class="foo">
     <h3>Progress steps</h3>
-    <p>Ana are mere ?</p>
-    <p>Nu se stie...</p>
-    <div style="width: 40px; height: 40px; background-color: forestgreen;" />
+    <pre>{message}</pre>
+
+    {#each steps as step, i}
+      <Step index={i + 1} />
+    {/each}
+
+    <div class="btns">
+      <button class="btn">&lt;</button>
+      <button class="btn">&gt;</button>
+    </div>
   </div>
 </div>
 
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Asap:wght@400;700&display=swap');
+  .foo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
   .content {
     width: 100%;
     height: 100%;
---
+++ src/Step.svelte
@@ -0,0 +1,48 @@
+<script>
+  export let index = 1
+  export let isCompleted = false
+  export let isCurrent = false
+
+  const size = 100
+  const colors = {
+    default: 'dimgray',
+    current: 'crimson',
+    completed: 'coral'
+  }
+</script>
+
+<div
+  class="step"
+  class:isCompleted
+  class:isCurrent
+  on:click
+  style="--size:{size}px;
+    --color-default:{colors.default};
+    --color-completed:{colors.completed};
+    --color-current:{colors.current};
+    --fs:{(size * 2) / 3}px"
+>
+  {index}
+</div>
+
+<style>
+  .step {
+    width: var(--size);
+    height: var(--size);
+    color: whitesmoke;
+    background-color: var(--color-default);
+    border-radius: var(--size);
+    font-weight: bold;
+    font-size: var(--fs);
+
+    text-align: center;
+    vertical-align: middle;
+    line-height: var(--size);
+  }
+  .isCompleted {
+    background-color: var(--color-completed);
+  }
+  .isCurrent {
+    background-color: var(--color-current);
+  }
+</style>
